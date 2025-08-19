8255627542639b5fdac84908c1b11903d71aa444
FetenFeyrer <72172103+FetenFeyrer@users.noreply.github.com>
2025-08-20 01:38:04 +0200

    implement audio reactive visuals with react-three

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AudioCircle from './AudioCircle.jsx'
+import AudioVisR3F from './AudioVisR3F.jsx'
 
 export default function App() {
-  return <AudioCircle />
+  return <AudioVisR3F />
 }
